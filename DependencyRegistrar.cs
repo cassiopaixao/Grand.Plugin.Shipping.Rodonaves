a82296e74d75d7473df73852e0e31349f8f42a59
Cássio Paixão <cassio.paixao@gmail.com>
2020-08-11 18:40:54 -0300

    Initial commit | Base shipping rate computation plugin structure

---
+++ DependencyRegistrar.cs
@@ -0,0 +1,17 @@
+﻿using Autofac;
+using Grand.Core.Configuration;
+using Grand.Core.Infrastructure;
+using Grand.Core.Infrastructure.DependencyManagement;
+
+namespace Grand.Plugin.Shipping.Rodonaves
+{
+    public class DependencyRegistrar : IDependencyRegistrar
+    {
+        public virtual void Register(ContainerBuilder builder, ITypeFinder typeFinder, GrandConfig grandConfig)
+        {
+            builder.RegisterType<RodonavesComputationMethod>().InstancePerLifetimeScope();
+        }
+
+        public int Order => 1;
+    }
+}
---
+++ RodonavesComputationMethod.cs
@@ -0,0 +1,44 @@
+﻿using Grand.Core.Domain.Orders;
+using Grand.Core.Plugins;
+using Grand.Services.Shipping;
+using Grand.Services.Shipping.Tracking;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grand.Plugin.Shipping.Rodonaves
+{
+    public class RodonavesComputationMethod : BasePlugin, IShippingRateComputationMethod
+    {
+        public ShippingRateComputationMethodType ShippingRateComputationMethodType => throw new NotImplementedException();
+
+        public IShipmentTracker ShipmentTracker => throw new NotImplementedException();
+
+        public Task<decimal?> GetFixedRate(GetShippingOptionRequest getShippingOptionRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetPublicViewComponent(out string viewComponentName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<GetShippingOptionResponse> GetShippingOptions(GetShippingOptionRequest getShippingOptionRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> HideShipmentMethods(IList<ShoppingCartItem> cart)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<string>> ValidateShippingForm(IFormCollection form)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
